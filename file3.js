879a9a1240587b1e9f92ff617ee2b3c286967c35
unknown <ajay766613@gmail.com>
2023-09-10 04:23:02 +0530

    swiper

--- file3.js
+++ file3.js
@@ -4,3 +4,12 @@ menubtn.onclick = () => {
     menubtn.classList.toggle('fa-times');
     navbar.classList.toggle('active');
 }
+
+var swiper = new Swiper(".home-slider", {
+    grabCursor: true,
+    loop: true,
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+});
