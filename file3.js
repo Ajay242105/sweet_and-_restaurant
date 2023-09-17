08ecf96bbef633d4379522b788a636ac55f7a65b
unknown <ajay766613@gmail.com>
2023-09-17 09:45:34 +0530

    food

--- file3.js
+++ file3.js
@@ -1,3 +1,4 @@
+// slider code is taken from swiper demos
 let navbar = document.querySelector('.header .navbar');
 let menubtn = document.querySelector('#menu-btn');
 menubtn.onclick = () => {
@@ -8,8 +9,33 @@ menubtn.onclick = () => {
 var swiper = new Swiper(".home-slider", {
     grabCursor: true,
     loop: true,
+    centeredSlides: true,
     navigation: {
         nextEl: ".swiper-button-next",
         prevEl: ".swiper-button-prev",
+    },
+});
+var swiper = new Swiper(".food-slider", {
+    grabCursor: true,
+    loop: true,
+    centeredSlides: true,
+    spaceBetween: 20,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    breakpoints: {
+        0: {
+            slidesPerView: 1,
+            // spaceBetween: 20,
+        },
+        700: {
+            slidesPerView: 2,
+            // spaceBetween: 40,
+        },
+        1000: {
+            slidesPerView: 3,
+            // spaceBetween: 50,
+        },
     },
 });
